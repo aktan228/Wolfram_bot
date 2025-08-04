743f59ce71dd460934c32f300f2d78e5a00bfed8
aktan228 <aktam200e@gmail.com>
2025-08-05 01:29:12 +0600

    added logic for modul

--- bot/wolfram_client.py
+++ bot/wolfram_client.py
@@ -6,4 +6,40 @@ def query_wolfram(query: str) -> str:
     app_id = os.getenv("WOLFRAM_APP_ID")
     url = "http://api.wolframalpha.com/v2/query"
 
+    params = {
+        "input":query,
+        "format":"plaintext,latex",
+        "output":"JSON",
+        "appid":app_id
+    }
+    res = requests.get(url,params=params).json()
     
+    try:
+        pods = res["queryresult"]["pods"]
+        for pod in pods:
+            if "plaintext" in pod["subpods"][0]:
+                return pod["subpods"][0]["plaintext"]
+        return "No results found"
+    except Exception:
+        return "Error processing in query"
+    
+    
+    
+    
+    # params = {
+    #     "input": query,
+    #     "format": "plaintext",
+    #     "output": "JSON",
+    #     "appid": app_id
+    # }
+
+    # res = requests.get(url, params=params).json()
+
+    # try:
+    #     pods = res["queryresult"]["pods"]
+    #     for pod in pods:
+    #         if "plaintext" in pod["subpods"][0]:
+    #             return pod["subpods"][0]["plaintext"]
+    #     return "No result found."
+    # except Exception:
+    #     return "Error processing the query."
