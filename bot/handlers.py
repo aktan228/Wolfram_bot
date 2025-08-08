7274aa5705199bc9cb04d49065b123e3ba81a86a
aktan228 <aktam200e@gmail.com>
2025-08-08 20:56:04 +0600

    almost finished

--- bot/handlers.py
+++ bot/handlers.py
@@ -2,6 +2,13 @@ from telegram import Update
 from telegram.ext import ContextTypes
 from wolfram_client import query_wolfram
 from .main import BOT_USERNAME
+from PIL import Image
+import pytesseract
+import os
+from io import BytesIO
+from latex_renderer import LatexRenderer
+
+renderer = LatexRenderer()
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello im the Wolfram Bot! How can i help you?")
@@ -10,6 +17,24 @@ async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def handle_query(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query_type = update.message.chat.type
     query = update.message.text
+    message= update.message
+    
+    if update.message.text:
+        query = update.message.text
+    elif update.message.photo:
+        photo_file = await update.message.photo[-1].get_file()
+        photo_bytes = await photo_file.download_as_bytearray()
+        
+        #OCR
+        img = Image.open(BytesIO(photo_bytes))
+        query = pytesseract.image_to_string(img)
+        
+        if not query.strip():
+            await update.message.reply_text('Couldnt recognize text on photo')
+            return
+    else:
+        await update.message.reply_text("Send text or photo with formula")
+        return
 
     print(f'User ({update.message.chat.id}) in {query_type}: "{query}"')
 
@@ -22,6 +47,16 @@ async def handle_query(update: Update, context: ContextTypes.DEFAULT_TYPE):
     else:
         result = query_wolfram(query)
 
+    if 'latex' in result:
+        filename = renderer.render(result['latex'])
+        with open(filename, 'rb') as img_file:
+            await message.reply_photo(img_file)
+        os.remove(filename)
+    elif "plaintext" in result:
+        await message.reply_text(result["plaintext"])
+        
+    else:
+        await message.reply_text(result.get("error","No result found"))
     print('Bot:', result)
     await update.message.reply_text(result) #    await update.message.reply_text(query_wolfram(update.message.reply_text))
 
